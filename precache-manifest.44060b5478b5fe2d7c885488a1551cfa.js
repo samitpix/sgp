3bb51decc8b64e0ca69beb972323be77804ded4c
samitpix <samitpix@gmail.com>
2022-05-14 10:17:58 +0430

    Updates

---
+++ precache-manifest.44060b5478b5fe2d7c885488a1551cfa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfab0a07d2b7da3530eccbde6b182ab",
+    "url": "/sgp/index.html"
+  },
+  {
+    "revision": "55da45848daf0d3ab1c5",
+    "url": "/sgp/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "5737dcc0bc4a668771c8",
+    "url": "/sgp/static/css/4.3c0635ab.chunk.css"
+  },
+  {
+    "revision": "57215bb3335d7105189f",
+    "url": "/sgp/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "2dfe5a418f1b544ab72c",
+    "url": "/sgp/static/css/7.751a6c49.chunk.css"
+  },
+  {
+    "revision": "dfed854dad0504fa0051",
+    "url": "/sgp/static/css/main.162c7196.chunk.css"
+  },
+  {
+    "revision": "55da45848daf0d3ab1c5",
+    "url": "/sgp/static/js/0.e4159552.chunk.js"
+  },
+  {
+    "revision": "f77df77b0064a06a0ced",
+    "url": "/sgp/static/js/3.c014426f.chunk.js"
+  },
+  {
+    "revision": "5737dcc0bc4a668771c8",
+    "url": "/sgp/static/js/4.5f66de21.chunk.js"
+  },
+  {
+    "revision": "57215bb3335d7105189f",
+    "url": "/sgp/static/js/5.b456db26.chunk.js"
+  },
+  {
+    "revision": "7d90f731adb96c7b86e3",
+    "url": "/sgp/static/js/6.91934930.chunk.js"
+  },
+  {
+    "revision": "2dfe5a418f1b544ab72c",
+    "url": "/sgp/static/js/7.965d4c56.chunk.js"
+  },
+  {
+    "revision": "dfed854dad0504fa0051",
+    "url": "/sgp/static/js/main.f2cd25d8.chunk.js"
+  },
+  {
+    "revision": "7cee3c8d28127cc72b34",
+    "url": "/sgp/static/js/runtime~main.b6118ed7.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/sgp/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/sgp/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/sgp/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/sgp/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
